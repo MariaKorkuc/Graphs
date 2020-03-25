51b4aba595169a520b267236ea502318468a879c
TheMoira <TheMoira@users.noreply.github.com>
2020-03-25 11:29:56 +0100

    cleaning files 1 (project2)

--- lab2 zd5/lab2zd5.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-//funkcja sprawdzajaca czy da sie stworzyc graf regularny o zadanej liczbie wierzcholkow i stopni
-bool isPossible(int n, int k);
-
-//generuje graf regularny o n wierzcholkach i stopniu k
-int** GenerateRegularGraph(int n, int k);
